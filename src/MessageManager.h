16b5f50318d2c58b594c896d29e734f4300aada4
Paulo Marinho <pakas_vieira_95@hotmail.com>
2023-12-04 14:40:22 -0300

    chore: remove useless code

--- src/MessageManager.h
+++
@@ -1,9 +0,0 @@
-#include <TouchScreen.h>
-
-class MessageManager {
-    public:
-        MessageManager(void);
-        void send(char *str);
-        void sendTSPoint(TSPoint);
-        void readString(char buffer[]);
-};
